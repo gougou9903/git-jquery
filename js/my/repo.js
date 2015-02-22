16ff140f20e92f6a996441d73c0f227249b49e6c
Haoran Fang <gougou9903@gmail.com>
2015-02-22 01:11:17 -0700

    build

--- js/my/repo.js
+++
@@ -1,7 +0,0 @@
-function myRepo(full_name){
-
-    // display a particular repo of mine in #details
-    // e.g., https://api.github.com/repos/doubleshow/up
-
-    alert('todo')
-}
--- js/my/repos.js
+++
@@ -1,7 +0,0 @@
-function myRepos(){
-
-    // display my repos in #list
-    // e.g., https://api.github.com/users/doubleshow/repos
-
-    alert('todo')
-}
